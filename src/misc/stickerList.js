79f810620472f2e4750a9b38cb3be43c66e5ea2f
XiangPingJun <XiangPingJun@users.noreply.github.com>
2017-05-21 18:06:49 +0800

    Update stickerList.js

--- src/misc/stickerList.js
+++ src/misc/stickerList.js
@@ -1,5 +1,5 @@
 export const stickerList = [
-  'u5orz70', 'utUCIOY', 'cJknRGT', 'EgkMgKH', '55IDDln', '2jCEhHf', 'qtaPFFl', '9exMGEW', 'SgkA5Tr', '41SGFaF', 'DaleIjH', 'HzxnRmA', 'CUgHzRU', 'AuDrgMP', '6H5wgx5', 'W2iny82', 'MDrz9Tg', '3nV127q', 'OvkUq4s', 'Qka24sE', 'QSltLPq', 'ipMZ4Y7', 'H8ViKxg', 'rJNgpPF', '4vH7SOV', 'wsnFo5Q', '5BW1pBv', '6jsPvrm'
+  'u5orz70', 'utUCIOY', 'cJknRGT', 'EgkMgKH', '55IDDln', '2jCEhHf', 'qtaPFFl', '9exMGEW', 'SgkA5Tr', '41SGFaF', 'DaleIjH', 'HzxnRmA', 'CUgHzRU', 'AuDrgMP', '6H5wgx5', 'W2iny82', 'MDrz9Tg', '3nV127q', 'OvkUq4s', 'Qka24sE', 'QSltLPq', 'ipMZ4Y7', 'H8ViKxg', 'rJNgpPF', '4vH7SOV', 'wsnFo5Q', '5BW1pBv', '6jsPvrm', 'kyTyQ9O'
 ]
 export const effectList = {
   BLAST: 'Begnt6Z',
