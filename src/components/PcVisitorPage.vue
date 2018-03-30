908634eda3f38164bc2e6b149363d5a5c2aa7dcf
Yuan-Hao Lai (RD-TW) <yuanhao_lai@tw.trendnet.org>
2018-03-30 08:08:50 +0800

    Nightsky BG

--- src/components/PcVisitorPage.vue
+++ src/components/PcVisitorPage.vue
@@ -1,9 +1,14 @@
 <template>
-  <div style="display:flex">
-    <dialog-box>
-      <div style="margin-bottom: 12px;">&gt; 新注音實況台</div>
-      <video-box :height="vidoeHeight" src="//www.youtube.com/embed/Xzv7eh2VaNI?enablejsapi=1"></video-box>
-    </dialog-box>
+  <div>
+    <background></background>
+    <div class="page-container">
+      <div class="middle">
+        <dialog-box>
+          <div style="margin-bottom: 12px;">&gt; 新注音實況台</div>
+          <video-box :height="videoHeight" :width="videoWidth" src="//www.youtube.com/embed/Xzv7eh2VaNI?enablejsapi=1"></video-box>
+        </dialog-box>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,20 +16,42 @@
 import Well from './Well.vue'
 import DialogBox from './DialogBox.vue'
 import VideoBox from './VideoBox.vue'
+import NightSkyBackground from './NightSkyBackground.vue'
 
 export default {
   data() {
     return {
-      vidoeHeight: 0
+      videoWidth: 0,
+      videoHeight: 0
     }
   },
   components: {
     well: Well,
     'dialog-box': DialogBox,
-    'video-box': VideoBox
+    'video-box': VideoBox,
+    'background': NightSkyBackground
   },
   created() {
-    this.vidoeHeight = document.documentElement.clientHeight - 100
+    const width = document.documentElement.clientWidth
+    const height = document.documentElement.clientHeight
+    console.log(width / height < 16 / 9)
+    if (width / height < 16 / 9)
+      this.videoWidth = width - 400
+    else
+      this.videoHeight = height - 120
   }
 }
 </script>
+
+<style scoped>
+.page-container {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+}
+.middle {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
