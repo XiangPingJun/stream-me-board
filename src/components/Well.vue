8299abd576b8611a429c45707e5584c26ab58f97
Yuan-Hao Lai (RD-TW) <yuanhao_lai@tw.trendnet.org>
2018-04-25 01:39:03 +0800

    working

--- src/components/Well.vue
+++ src/components/Well.vue
@@ -17,17 +17,20 @@
   box-shadow: 0 -4px #3d3933, -4px 0 #3d3933, 0 4px #3d3933, 4px 0 #3d3933,
     -4px -4px #2a2e30, -4px 4px #2a2e30, 4px -4px #2a2e30, 4px 4px #2a2e30;
   box-sizing: border-box;
+  transition-duration: 0.25s;
 }
 .border {
   padding: 4px;
   box-shadow: 0 -4px #2a2e30, -4px 0 #2a2e30, 0 4px #2a2e30, 4px 0 #2a2e30,
     -4px -4px #705749, -4px 4px #705749, 4px -4px #705749, 4px 4px #705749;
   box-sizing: border-box;
+  transition-duration: 0.25s;
 }
 .shadow {
   padding: 4px;
   box-shadow: 0 -4px #86674d, -4px 0 #86674d, 0 4px #86674d, 4px 0 #86674d;
   box-sizing: border-box;
   margin: 8px 0px;
+  transition-duration: 0.25s;
 }
 </style>
