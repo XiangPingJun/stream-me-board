c1874ee47a904542335c2e574b02635489198807
Yuan-Hao Lai (RD-TW) <yuanhao_lai@trend.com.tw>
2018-07-27 11:27:33 +0800

    quiz

--- functions/src/quiz/getQuiz.js
+++ functions/src/quiz/getQuiz.js
@@ -1,53 +1,25 @@
-const request = require("request")
 const fs = require('fs')
+const ans = require('./answer.js')
 
 let result = JSON.parse(fs.readFileSync('index.ts', 'utf-8').replace('export default ', ''))
-let lastLine = ''
-let question
 let j = 0
 fs.readFileSync('toParse.txt', 'utf-8').split('\n').forEach(line => {
-	line = line.trim()
-	switch (lastLine) {
-		case 'NUMBER':
-			question += line
-			lastLine = 'QUESTION'
-			break
-		case 'QUESTION':
-			let items = line.split('、').map(item => ({ text: item }))
-			items = shuffle(items)
-			items = items.map((item, i) => ({ code: String.fromCharCode(65 + i), ...item }))
-			const correct = line.split('、').map(correctItem => items.findIndex(item => item.text == correctItem))
-				.map(idx => String.fromCharCode(65 + idx))
-			for (let i = 0, item; item = items[i]; i++)
-				question += ` ${item.code}: ${item.text}`
-			const correctIdx = Math.floor(Math.random() * items.length)
-			let ops = []
-			for (let i = 0; i < correct.length; i++) {
-				if (correctIdx == i) {
-					ops[i] = correct.join(' → ')
-				} else {
-					let found = false
-					let candidate
-					do {
-						candidate = shuffle(correct).join(' → ')
-						found = ops.indexOf(candidate) != -1
-					} while (found)
-					ops[i] = candidate
-				}
-			}
-			result[question] = { OP: ops, A: correctIdx }
-			lastLine = 'OPTIONS'
-			break
-		default:
-			question = ''
-			lastLine = 'NUMBER'
+	if (-1 != line.indexOf('以上') || -1 != line.indexOf('以下') || -1 != line.indexOf('承上') || -1 != line.indexOf('圖') || -1 != line.indexOf('皆是') || -1 != line.indexOf('皆非'))
+		return
+	const arr = line.trim().match(/^(\d+)\.(.+)Ａ\.(.+)Ｂ\.(.+)Ｃ\.(.+)Ｄ\.(.+)/)
+	if (!arr)
+		return
+	const Q = arr[2] + ':'
+	result[Q] = {
+		A: ans[arr[1] + ''],
+		OP: [arr[3], arr[4], arr[5], arr[6]]
 	}
 })
 for (let i in result) {
-	if (-1 == result[i].A || isNaN(result[i].A) || 4 != result[i].OP.length || i.indexOf('我國') >= 0) {
+	if (-1 != result[i].OP.findIndex(op => -1 !== op.indexOf('皆是') || -1 !== op.indexOf('皆非')))
 		delete result[i]
-		continue
-	}
+	else if (undefined == result[i].A)
+		delete result[i]
 }
 console.log(`Total ${Object.keys(result).length} questions`)
 fs.writeFileSync('index.ts', 'export default ' + JSON.stringify(result), 'utf-8')
