2d1044266f10922e884bcffd4395b0c70104b7f6
Yuan-Hao Lai (RD-TW) <yuanhao_lai@trend.com.tw>
2018-07-30 14:32:54 +0800

    Auto stash before merge of "master" and "develop/refactor"

---
+++ static/firebase-conf.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+	apiKey: "AIzaSyA1ParJnwEFwshfvcZxtMR5G5q4862NCqI",
+	authDomain: "xiang-ping-jun.firebaseapp.com",
+	databaseURL: "https://xiang-ping-jun.firebaseio.com",
+	projectId: "xiang-ping-jun",
+	storageBucket: "xiang-ping-jun.appspot.com",
+	messagingSenderId: "205831146018"
+})
