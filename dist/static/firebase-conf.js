6c707b8f1512e4231f860eb9e80d63aee95f0263
Yuan-Hao Lai (RD-TW) <yuanhao_lai@trend.com.tw>
2018-09-12 15:53:59 +0800

    remove dist

--- dist/static/firebase-conf.js
+++
@@ -1,8 +0,0 @@
-firebase.initializeApp({
-	apiKey: "AIzaSyA1ParJnwEFwshfvcZxtMR5G5q4862NCqI",
-	authDomain: "xiang-ping-jun.firebaseapp.com",
-	databaseURL: "https://xiang-ping-jun.firebaseio.com",
-	projectId: "xiang-ping-jun",
-	storageBucket: "xiang-ping-jun.appspot.com",
-	messagingSenderId: "205831146018"
-})
